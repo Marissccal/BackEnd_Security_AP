9577f51e1310af6dcdb399b137ff3ebddd7ad035
unknown <explotacionesalfa@gmail.com>
2022-05-26 20:19:20 -0300

    cambios varios

--- target/generated-sources/annotations/com/portfoliosecurity/argprog/mapper/UserMapperImpl.java
+++ target/generated-sources/annotations/com/portfoliosecurity/argprog/mapper/UserMapperImpl.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-26T11:57:22-0300",
+    date = "2022-05-26T20:17:42-0300",
     comments = "version: 1.4.2.Final, compiler: Eclipse JDT (IDE) 1.4.100.v20220318-0906, environment: Java 17.0.2 (Eclipse Adoptium)"
 )
 @Component
